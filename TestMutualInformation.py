4ff3cba5b003ad78528c958d2f63d3b982fc170a
Ran Moshe <rmoshe@oath.com>
2018-08-27 09:49:39 +0300

    Mutual information

--- TestMutualInformation.py
+++ TestMutualInformation.py
@@ -9,7 +9,7 @@ class TestMutualInformation(unittest.TestCase):
     def test_probability_calc_one_column(self):
         matrix = pd.read_csv('test_files/test_probability_calc.csv', dtype=np.str)
         samples = SampleSet(matrix)
-        a_prob = samples.get_probability(['a'])
+        a_prob = samples.probability(['a'])
         self.assertEqual(a_prob['catA'], 0.3)
         self.assertEqual(a_prob['catB'], 0.25)
         self.assertEqual(a_prob['catC'], 0.45)
@@ -17,15 +17,22 @@ class TestMutualInformation(unittest.TestCase):
     def test_probability_calc_shuffled_columns(self):
         matrix = pd.read_csv('test_files/test_probability_calc_shuffled_columns.csv', dtype=np.str)
         samples = SampleSet(matrix)
-        a_prob = samples.get_probability(['b', 'a'])
+        a_prob = samples.probability(['b', 'a'])
         self.assertEqual(a_prob['1,catA'], 0.2)
         self.assertEqual(a_prob['2,catA'], 0.1)
 
-    def test_pure_set(self):
-        pass
+    def test_entropy(self):
+        matrix = pd.read_csv('test_files/test_entropy.csv', dtype=np.str)
+        samples = SampleSet(matrix)
+        self.assertEqual(samples.entropy(['c']), 0)
+        self.assertEqual(samples.entropy(['a']), 1)
+        self.assertEqual(samples.entropy(['c','a']), 1)
 
     def test_a_b_mutual_info(self):
-        pass
+        matrix = pd.read_csv('test_files/test_mutual_information.csv', dtype=np.str)
+        samples = SampleSet(matrix)
+        self.assertEqual(samples.mutual_information(['a'], ['b']), 1)
+        
 
 
 if __name__ == '__main__':
--- sample_set.py
+++ sample_set.py
@@ -1,12 +1,18 @@
 '''
-Description of the program
+A class for statistical calculations on a sample set.
+Its input is a pandas.DataFrame whose columns are all of numpy.str type and are categorical.
 '''
+
+import numpy as np
 
 class SampleSet():
     def __init__(self, matrix):
         self.matrix = matrix
 
-    def get_probability(self, columns):
+    def probability(self, columns):
+        '''
+        Joint probability for a list of column names
+        '''
         sub_matrix = self.matrix[columns]
         combo_count = {}
         for sub_row in sub_matrix.values:
@@ -22,5 +28,36 @@ class SampleSet():
 
         return joint_probability
 
+    def entropy(self, columns):
+        '''
+        Entropy for a list of column names
+        '''
+        jp = self.probability(columns)
+        entropy = 0
+        for val in jp.values():
+            entropy += -val*np.log2(val)
 
-        
+        return entropy
+
+    def A_given_B_value(self, groupA, groupB, B_val):
+        vals = B_val.split(',')
+        zipped = zip(groupB, vals)
+        val_per_column = [x for x in zipped]
+        queries = ['{col} =="{val}"'.format(col=x[0], val=x[1]) for x in val_per_column]
+        query = ' & '.join(queries)
+        return self.matrix.query(query)[groupA]
+
+    def mutual_information(self, groupA, groupB):
+        '''
+        Mutual information between columns in groupA and columns in groupB
+        '''
+        entropyA = self.entropy(groupA)
+        probB = self.probability(groupB)
+        mi = entropyA
+        for B_category, B_probability in probB.items():
+            subA = SampleSet(self.A_given_B_value(groupA, groupB, B_category))
+            subA_entropy = subA.entropy(groupA)
+            mi = mi - B_probability*subA_entropy
+
+        return mi
+
