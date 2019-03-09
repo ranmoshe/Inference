1599fc8d4a51bc640917ca18017ee7583484561f
Ran Moshe <rmoshe@verizonmedia.com>
2019-03-09 19:50:29 +0200

    Handling common cause correctly

--- test_graph.py
+++ test_graph.py
@@ -73,6 +73,64 @@ class TestGraph(unittest.TestCase):
 
         return choice
 
+    def season_to_flu_epidemic(self, season):
+        if season == 'winter':
+            weights = [0.05, 0.95]
+        elif season == 'spring':
+            weights = [0.005, 0.995]
+        elif season == 'summer':
+            weights = [0.0005, 0.9995]
+        elif season == 'fall':
+            weights = [0.03, 0.97]
+
+        choice = random.choices(
+                population=['yes', 'no'],
+                weights = weights,
+                k = 1)[0]
+
+        return choice
+
+    def season_to_temperature(self, season):
+        if season == 'winter':
+            weights = [0.6, 0.3, 0.1]
+        elif season == 'spring':
+            weights = [0.15, 0.6, 0.25]
+        elif season == 'summer':
+            weights = [0.1, 0.3, 0.6]
+        elif season == 'fall':
+            weights = [0.25, 0.6, 0.15]
+
+        choice = random.choices(
+                population=['cold', 'mild', 'hot'],
+                weights = weights,
+                k = 1)[0]
+
+        return choice
+
+    def test_common_cause(self):
+        '''
+        Season affects both the temperature and the chance for a flu epidemic.
+        The algorithm can't determine causal relationship by this data, so we expect an undirected chain.
+        '''
+
+        season_series = generate_random_series(10000, 'season', [0.35, 0.15, 0.35])
+        df = pd.DataFrame({'season': season_series})
+        df['season'] = df['season'].map({'season_0': 'winter', 'season_1': 'spring','season_2': 'summer', 'season_3': 'fall'})
+        df['temperature'] = df.apply(lambda row: self.season_to_temperature(row['season']), axis=1)
+        df['flu_epidemic'] = df.apply(lambda row: self.season_to_flu_epidemic(row['season']), axis=1)
+        ic_graph = IC_Graph(SampleSet(df))
+        ic_graph.build_graph()
+        directed = [t for t in ic_graph.graph.edges.data('out') if t[2] is not None]
+        import ipdb; ipdb.set_trace()
+
+        self.assertEqual(
+                [],
+                directed
+                )
+        self.assertEqual(
+                [('season', 'temperature'), ('season', 'flu_epidemic')], 
+                [t for t in ic_graph.graph.edges])
+     
     def test_3_node_chain(self):
         '''
         We have a dog, a cat and a mouse. 
@@ -80,7 +138,7 @@ class TestGraph(unittest.TestCase):
         If the cat is left alone, it can chase the mouse, and then the mouse would mostly run.
         The algorithm can't determine causal relationship by this data, so we expect an undirected chain.
         '''
-        dog_series = generate_random_series(1000, 'dog', [0.2])
+        dog_series = generate_random_series(10000, 'dog', [0.2])
         df = pd.DataFrame({'dog': dog_series})
         df['dog'] = df['dog'].map({'dog_0': 'resting', 'dog_1': 'running'})
         df['cat'] = df.apply(lambda row: self.hunted_state(row['dog'], 0.8, 0.2), axis=1)
